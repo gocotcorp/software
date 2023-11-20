d93204801e46d29eba76f2c37f66f68b9502682b
gocotcorp <151520988+gocotcorp@users.noreply.github.com>
2023-11-20 15:50:47 +0530

    Add files via upload

---
+++ platform/mobile/browser/main.js
@@ -0,0 +1,555 @@
+document.getElementById("go").addEventListener("click", function(){
+  
+  var i = document.getElementById("inp").value;
+  if (i.includes("http://")){
+    
+    document.getElementById("cache").innerHTML = "<a href='" + i + "' id='pro'></a>";
+    document.getElementById("pro").click();
+    document.getElementById("cache").innerHTML = "";
+    
+    
+  }
+  else if (i.includes("https://")) {
+  
+    document.getElementById("cache").innerHTML = "<a href='" + i + "' id='pro'></a>";
+    document.getElementById("pro").click();
+    document.getElementById("cache").innerHTML = "";
+  
+  
+  }
+  else if (i.includes("www.")) {
+  
+    document.getElementById("cache").innerHTML = "<a href='https://" + i + "' id='pro'></a>";
+    document.getElementById("pro").click();
+    document.getElementById("cache").innerHTML = "";
+  
+  
+  }
+  else if (i.includes(".com")) {
+  
+    if (i.includes("https://")){
+      
+      document.getElementById("cache").innerHTML = "<a href='" + i + "' id='pro'></a>";
+      document.getElementById("pro").click();
+      document.getElementById("cache").innerHTML = "";
+      
+    }
+    else if (i.includes("http://")) {
+    
+      document.getElementById("cache").innerHTML = "<a href='" + i + "' id='pro'></a>";
+      document.getElementById("pro").click();
+      document.getElementById("cache").innerHTML = "";
+    
+    }
+    else {
+      
+      document.getElementById("cache").innerHTML = "<a href='https://" + i + "' id='pro'></a>";
+      document.getElementById("pro").click();
+      document.getElementById("cache").innerHTML = "";
+      
+    }
+  
+  
+  }
+  else if (i.includes(".io")) {
+  
+    if (i.includes("https://")) {
+    
+      document.getElementById("cache").innerHTML = "<a href='" + i + "' id='pro'></a>";
+      document.getElementById("pro").click();
+      document.getElementById("cache").innerHTML = "";
+    
+    }
+    else if (i.includes("http://")) {
+    
+      document.getElementById("cache").innerHTML = "<a href='" + i + "' id='pro'></a>";
+      document.getElementById("pro").click();
+      document.getElementById("cache").innerHTML = "";
+    
+    }
+    else {
+    
+      document.getElementById("cache").innerHTML = "<a href='https://" + i + "' id='pro'></a>";
+      document.getElementById("pro").click();
+      document.getElementById("cache").innerHTML = "";
+    
+    }
+  
+  
+  }
+  
+  else if (i.includes(".org")) {
+  
+    if (i.includes("https://")) {
+    
+      document.getElementById("cache").innerHTML = "<a href='" + i + "' id='pro'></a>";
+      document.getElementById("pro").click();
+      document.getElementById("cache").innerHTML = "";
+    
+    }
+    else if (i.includes("http://")) {
+    
+      document.getElementById("cache").innerHTML = "<a href='" + i + "' id='pro'></a>";
+      document.getElementById("pro").click();
+      document.getElementById("cache").innerHTML = "";
+    
+    }
+    else {
+    
+      document.getElementById("cache").innerHTML = "<a href='https://" + i + "' id='pro'></a>";
+      document.getElementById("pro").click();
+      document.getElementById("cache").innerHTML = "";
+    
+    }
+  
+  
+  }
+  else if (i.includes(".net")) {
+  
+    if (i.includes("https://")) {
+    
+      document.getElementById("cache").innerHTML = "<a href='" + i + "' id='pro'></a>";
+      document.getElementById("pro").click();
+      document.getElementById("cache").innerHTML = "";
+    
+    }
+    else if (i.includes("http://")) {
+    
+      document.getElementById("cache").innerHTML = "<a href='" + i + "' id='pro'></a>";
+      document.getElementById("pro").click();
+      document.getElementById("cache").innerHTML = "";
+    
+    }
+    else {
+    
+      document.getElementById("cache").innerHTML = "<a href='https://" + i + "' id='pro'></a>";
+      document.getElementById("pro").click();
+      document.getElementById("cache").innerHTML = "";
+    
+    }
+  
+  
+  }
+  else if (i.includes(".new")) {
+  
+    if (i.includes("https://")) {
+    
+      document.getElementById("cache").innerHTML = "<a href='" + i + "' id='pro'></a>";
+      document.getElementById("pro").click();
+      document.getElementById("cache").innerHTML = "";
+    
+    }
+    else if (i.includes("http://")) {
+    
+      document.getElementById("cache").innerHTML = "<a href='" + i + "' id='pro'></a>";
+      document.getElementById("pro").click();
+      document.getElementById("cache").innerHTML = "";
+    
+    }
+    else {
+    
+      document.getElementById("cache").innerHTML = "<a href='https://" + i + "' id='pro'></a>";
+      document.getElementById("pro").click();
+      document.getElementById("cache").innerHTML = "";
+    
+    }
+  
+  
+  }
+  else if (i.includes(".blog")) {
+  
+    if (i.includes("https://")) {
+    
+      document.getElementById("cache").innerHTML = "<a href='" + i + "' id='pro'></a>";
+      document.getElementById("pro").click();
+      document.getElementById("cache").innerHTML = "";
+    
+    }
+    else if (i.includes("http://")) {
+    
+      document.getElementById("cache").innerHTML = "<a href='" + i + "' id='pro'></a>";
+      document.getElementById("pro").click();
+      document.getElementById("cache").innerHTML = "";
+    
+    }
+    else {
+    
+      document.getElementById("cache").innerHTML = "<a href='https://" + i + "' id='pro'></a>";
+      document.getElementById("pro").click();
+      document.getElementById("cache").innerHTML = "";
+    
+    }
+  
+  
+  }
+  else if (i.includes(".in")) {
+  
+    if (i.includes("https://")) {
+    
+      document.getElementById("cache").innerHTML = "<a href='" + i + "' id='pro'></a>";
+      document.getElementById("pro").click();
+      document.getElementById("cache").innerHTML = "";
+    
+    }
+    else if (i.includes("http://")) {
+    
+      document.getElementById("cache").innerHTML = "<a href='" + i + "' id='pro'></a>";
+      document.getElementById("pro").click();
+      document.getElementById("cache").innerHTML = "";
+    
+    }
+    else {
+    
+      document.getElementById("cache").innerHTML = "<a href='https://" + i + "' id='pro'></a>";
+      document.getElementById("pro").click();
+      document.getElementById("cache").innerHTML = "";
+    
+    }
+  
+  
+  }
+  else if (i.includes(".us")) {
+  
+    if (i.includes("https://")) {
+    
+      document.getElementById("cache").innerHTML = "<a href='" + i + "' id='pro'></a>";
+      document.getElementById("pro").click();
+      document.getElementById("cache").innerHTML = "";
+    
+    }
+    else if (i.includes("http://")) {
+    
+      document.getElementById("cache").innerHTML = "<a href='" + i + "' id='pro'></a>";
+      document.getElementById("pro").click();
+      document.getElementById("cache").innerHTML = "";
+    
+    }
+    else {
+    
+      document.getElementById("cache").innerHTML = "<a href='https://" + i + "' id='pro'></a>";
+      document.getElementById("pro").click();
+      document.getElementById("cache").innerHTML = "";
+    
+    }
+  
+  
+  }
+  else if (i.includes(".co")) {
+  
+    if (i.includes("https://")) {
+    
+      document.getElementById("cache").innerHTML = "<a href='" + i + "' id='pro'></a>";
+      document.getElementById("pro").click();
+      document.getElementById("cache").innerHTML = "";
+    
+    }
+    else if (i.includes("http://")) {
+    
+      document.getElementById("cache").innerHTML = "<a href='" + i + "' id='pro'></a>";
+      document.getElementById("pro").click();
+      document.getElementById("cache").innerHTML = "";
+    
+    }
+    else {
+    
+      document.getElementById("cache").innerHTML = "<a href='https://" + i + "' id='pro'></a>";
+      document.getElementById("pro").click();
+      document.getElementById("cache").innerHTML = "";
+    
+    }
+  
+  
+  }
+  else {
+    // https://www.google.com/search?q=hy
+    document.getElementById("cache").innerHTML = "<a href='https://www.google.com/search?q=" + i + "' id='pro'></a>";
+    document.getElementById("pro").click();
+    document.getElementById("cache").innerHTML = "";
+    
+  }
+  
+  
+})
+
+
+// enter key press
+document.onkeydown = function(event) {
+  if (event.keyCode === 13) {
+    // The Enter key was pressed.
+    
+      var i = document.getElementById("inp").value;
+  if (i.includes("http://")){
+    
+    document.getElementById("cache").innerHTML = "<a href='" + i + "' id='pro'></a>";
+    document.getElementById("pro").click();
+    document.getElementById("cache").innerHTML = "";
+    
+    
+  }
+  else if (i.includes("https://")) {
+  
+    document.getElementById("cache").innerHTML = "<a href='" + i + "' id='pro'></a>";
+    document.getElementById("pro").click();
+    document.getElementById("cache").innerHTML = "";
+  
+  
+  }
+  else if (i.includes("www.")) {
+  
+    document.getElementById("cache").innerHTML = "<a href='https://" + i + "' id='pro'></a>";
+    document.getElementById("pro").click();
+    document.getElementById("cache").innerHTML = "";
+  
+  
+  }
+  else if (i.includes(".com")) {
+  
+    if (i.includes("https://")){
+      
+      document.getElementById("cache").innerHTML = "<a href='" + i + "' id='pro'></a>";
+      document.getElementById("pro").click();
+      document.getElementById("cache").innerHTML = "";
+      
+    }
+    else if (i.includes("http://")) {
+    
+      document.getElementById("cache").innerHTML = "<a href='" + i + "' id='pro'></a>";
+      document.getElementById("pro").click();
+      document.getElementById("cache").innerHTML = "";
+    
+    }
+    else {
+      
+      document.getElementById("cache").innerHTML = "<a href='https://" + i + "' id='pro'></a>";
+      document.getElementById("pro").click();
+      document.getElementById("cache").innerHTML = "";
+      
+    }
+  
+  
+  }
+  else if (i.includes(".io")) {
+  
+    if (i.includes("https://")) {
+    
+      document.getElementById("cache").innerHTML = "<a href='" + i + "' id='pro'></a>";
+      document.getElementById("pro").click();
+      document.getElementById("cache").innerHTML = "";
+    
+    }
+    else if (i.includes("http://")) {
+    
+      document.getElementById("cache").innerHTML = "<a href='" + i + "' id='pro'></a>";
+      document.getElementById("pro").click();
+      document.getElementById("cache").innerHTML = "";
+    
+    }
+    else {
+    
+      document.getElementById("cache").innerHTML = "<a href='https://" + i + "' id='pro'></a>";
+      document.getElementById("pro").click();
+      document.getElementById("cache").innerHTML = "";
+    
+    }
+  
+  
+  }
+  
+  else if (i.includes(".org")) {
+  
+    if (i.includes("https://")) {
+    
+      document.getElementById("cache").innerHTML = "<a href='" + i + "' id='pro'></a>";
+      document.getElementById("pro").click();
+      document.getElementById("cache").innerHTML = "";
+    
+    }
+    else if (i.includes("http://")) {
+    
+      document.getElementById("cache").innerHTML = "<a href='" + i + "' id='pro'></a>";
+      document.getElementById("pro").click();
+      document.getElementById("cache").innerHTML = "";
+    
+    }
+    else {
+    
+      document.getElementById("cache").innerHTML = "<a href='https://" + i + "' id='pro'></a>";
+      document.getElementById("pro").click();
+      document.getElementById("cache").innerHTML = "";
+    
+    }
+  
+  
+  }
+  else if (i.includes(".net")) {
+  
+    if (i.includes("https://")) {
+    
+      document.getElementById("cache").innerHTML = "<a href='" + i + "' id='pro'></a>";
+      document.getElementById("pro").click();
+      document.getElementById("cache").innerHTML = "";
+    
+    }
+    else if (i.includes("http://")) {
+    
+      document.getElementById("cache").innerHTML = "<a href='" + i + "' id='pro'></a>";
+      document.getElementById("pro").click();
+      document.getElementById("cache").innerHTML = "";
+    
+    }
+    else {
+    
+      document.getElementById("cache").innerHTML = "<a href='https://" + i + "' id='pro'></a>";
+      document.getElementById("pro").click();
+      document.getElementById("cache").innerHTML = "";
+    
+    }
+  
+  
+  }
+  else if (i.includes(".new")) {
+  
+    if (i.includes("https://")) {
+    
+      document.getElementById("cache").innerHTML = "<a href='" + i + "' id='pro'></a>";
+      document.getElementById("pro").click();
+      document.getElementById("cache").innerHTML = "";
+    
+    }
+    else if (i.includes("http://")) {
+    
+      document.getElementById("cache").innerHTML = "<a href='" + i + "' id='pro'></a>";
+      document.getElementById("pro").click();
+      document.getElementById("cache").innerHTML = "";
+    
+    }
+    else {
+    
+      document.getElementById("cache").innerHTML = "<a href='https://" + i + "' id='pro'></a>";
+      document.getElementById("pro").click();
+      document.getElementById("cache").innerHTML = "";
+    
+    }
+  
+  
+  }
+  else if (i.includes(".blog")) {
+  
+    if (i.includes("https://")) {
+    
+      document.getElementById("cache").innerHTML = "<a href='" + i + "' id='pro'></a>";
+      document.getElementById("pro").click();
+      document.getElementById("cache").innerHTML = "";
+    
+    }
+    else if (i.includes("http://")) {
+    
+      document.getElementById("cache").innerHTML = "<a href='" + i + "' id='pro'></a>";
+      document.getElementById("pro").click();
+      document.getElementById("cache").innerHTML = "";
+    
+    }
+    else {
+    
+      document.getElementById("cache").innerHTML = "<a href='https://" + i + "' id='pro'></a>";
+      document.getElementById("pro").click();
+      document.getElementById("cache").innerHTML = "";
+    
+    }
+  
+  
+  }
+  else if (i.includes(".in")) {
+  
+    if (i.includes("https://")) {
+    
+      document.getElementById("cache").innerHTML = "<a href='" + i + "' id='pro'></a>";
+      document.getElementById("pro").click();
+      document.getElementById("cache").innerHTML = "";
+    
+    }
+    else if (i.includes("http://")) {
+    
+      document.getElementById("cache").innerHTML = "<a href='" + i + "' id='pro'></a>";
+      document.getElementById("pro").click();
+      document.getElementById("cache").innerHTML = "";
+    
+    }
+    else {
+    
+      document.getElementById("cache").innerHTML = "<a href='https://" + i + "' id='pro'></a>";
+      document.getElementById("pro").click();
+      document.getElementById("cache").innerHTML = "";
+    
+    }
+  
+  
+  }
+  else if (i.includes(".us")) {
+  
+    if (i.includes("https://")) {
+    
+      document.getElementById("cache").innerHTML = "<a href='" + i + "' id='pro'></a>";
+      document.getElementById("pro").click();
+      document.getElementById("cache").innerHTML = "";
+    
+    }
+    else if (i.includes("http://")) {
+    
+      document.getElementById("cache").innerHTML = "<a href='" + i + "' id='pro'></a>";
+      document.getElementById("pro").click();
+      document.getElementById("cache").innerHTML = "";
+    
+    }
+    else {
+    
+      document.getElementById("cache").innerHTML = "<a href='https://" + i + "' id='pro'></a>";
+      document.getElementById("pro").click();
+      document.getElementById("cache").innerHTML = "";
+    
+    }
+  
+  
+  }
+  else if (i.includes(".co")) {
+  
+    if (i.includes("https://")) {
+    
+      document.getElementById("cache").innerHTML = "<a href='" + i + "' id='pro'></a>";
+      document.getElementById("pro").click();
+      document.getElementById("cache").innerHTML = "";
+    
+    }
+    else if (i.includes("http://")) {
+    
+      document.getElementById("cache").innerHTML = "<a href='" + i + "' id='pro'></a>";
+      document.getElementById("pro").click();
+      document.getElementById("cache").innerHTML = "";
+    
+    }
+    else {
+    
+      document.getElementById("cache").innerHTML = "<a href='https://" + i + "' id='pro'></a>";
+      document.getElementById("pro").click();
+      document.getElementById("cache").innerHTML = "";
+    
+    }
+  
+  
+  }
+  else {
+    // https://www.google.com/search?q=hy
+    document.getElementById("cache").innerHTML = "<a href='https://www.google.com/search?q=" + i + "' id='pro'></a>";
+    document.getElementById("pro").click();
+    document.getElementById("cache").innerHTML = "";
+    
+  }
+  
+  
+
+    
+  }
+};
+
+
+  
